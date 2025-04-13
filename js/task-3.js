bb7f3879373e468be8bc3673892cfd317f493fd2
Anastasiia Sukhai <asyasyka@gmail.com>
2025-04-13 12:54:37 +0300

    Коммит изменений 3 задания

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,13 @@
 
+
 function getElementWidth(content, padding, border) {
-    return parseFloat(content);
+  const contentValue = parseFloat(content);
+  const paddingValue = parseFloat(padding) * 2;
+  const borderValue = parseFloat(border) * 2;
+
+  return contentValue + paddingValue + borderValue;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px")); 
